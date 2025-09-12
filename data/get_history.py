74de282f611c426496cb82bd6ac29bfb162ce98a
Huong <huong@Thuy-Nguyen.local>
2025-09-12 14:55:40 +0700

    new version

--- data/get_history.py
+++ data/get_history.py
@@ -9,7 +9,7 @@ SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-def get_latest_messages(limit=10):
+def get_latest_messages(user_id, limit=10):
     """
     Lấy `limit` records lịch sử chat gần nhất từ bảng messages_test.
     Trả về list các dict, mỗi dict là một bản ghi.
@@ -18,6 +18,7 @@ def get_latest_messages(limit=10):
         response = (
             supabase.table("messages_test")
             .select("*")
+            .eq("user_id", user_id)
             .order("created_at", desc=True)
             .limit(limit)
             .execute()
@@ -27,7 +28,25 @@ def get_latest_messages(limit=10):
         print("Lỗi khi lấy lịch sử chat:", e)
         return []  
     
+def get_all_messages(user_id):
+    """
+    Lấy tất cả records từ bảng messages_test.
+    Trả về list các dict, mỗi dict là một bản ghi.
+    """
+    try:
+        response = (
+            supabase.table("messages_test")
+            .select("message", "reply")
+            .eq("user_id", user_id)
+            # .order("created_at", asc=True)
+            .execute()
+        )
+        return response.data if hasattr(response, "data") else []
+    except Exception as e:
+        print("Lỗi khi lấy tất cả tin nhắn:", e)
+        return []
+    
 if __name__ == "__main__":
-   history = get_latest_messages(8)
+   history = get_all_messages(user_id="d3f893c7-2751-40f3-9bb4-b201ac8987a0")
    for record in history:
        print(record)
